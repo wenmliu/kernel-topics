1bfb65d181dbb290f9c062c000c452c5cbe268b1
Hangxiang Ma <hangxiang.ma@oss.qualcomm.com>
2026-05-08 02:51:07 -0700

    FROMLIST: media: qcom: camss: vfe: Add support for VFE 980
    Add support for Video Front End (VFE) that is on the SM8750 SoCs. VFE
    gen4 has support for VFE 980. This change limits SM8750 VFE output lines
    to 3 for now as constrained by the CAMSS driver framework.
    
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Co-developed-by: Atiya Kailany <atiya.kailany@oss.qualcomm.com>
    Signed-off-by: Atiya Kailany <atiya.kailany@oss.qualcomm.com>
    Signed-off-by: Hangxiang Ma <hangxiang.ma@oss.qualcomm.com>
    Link: https://lore.kernel.org/all/20260508-add-support-for-camss-on-sm8750-v3-5-fc6861a65c67@oss.qualcomm.com/

--- drivers/media/platform/qcom/camss/camss-vfe.c
+++ drivers/media/platform/qcom/camss/camss-vfe.c
@@ -351,6 +351,7 @@ static u32 vfe_src_pad_code(struct vfe_line *line, u32 sink_code,
 	case CAMSS_845:
 	case CAMSS_8550:
 	case CAMSS_8650:
+	case CAMSS_8750:
 	case CAMSS_8775P:
 	case CAMSS_KAANAPALI:
 	case CAMSS_X1E80100:
@@ -2014,6 +2015,7 @@ static int vfe_bpl_align(struct vfe_device *vfe)
 	case CAMSS_845:
 	case CAMSS_8550:
 	case CAMSS_8650:
+	case CAMSS_8750:
 	case CAMSS_8775P:
 	case CAMSS_KAANAPALI:
 	case CAMSS_X1E80100:
